a050acf181a97968d712865458de4f41e8a90279
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-09-24 12:45:12 -0300

    Initial Commit

---
+++ FormWithControls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FormWithControls
+//
+//  Created by Rafael Gonzaga on 24/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FormWithControls/FormWithControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormWithControlsApp.swift
+//  FormWithControls
+//
+//  Created by Rafael Gonzaga on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FormWithControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
